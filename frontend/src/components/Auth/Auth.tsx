b0ae45c1905863e1ba9653a2e28efd866cec0652
Aditya-Sakpal <aditya.as@somaiya.edu>
2024-12-21 17:58:03 +0530

    changed the endpoint

--- frontend/src/components/Auth/Auth.tsx
+++ frontend/src/components/Auth/Auth.tsx
@@ -11,7 +11,7 @@ const Auth: React.FC = () => {
         event.preventDefault();
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/login', {
+            const response = await fetch('https://ifrs-17-backend-d8zs.vercel.app/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
